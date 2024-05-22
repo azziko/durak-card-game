2953ca398509fb0f6837f2a569f4f0227513d2a4
Azziko <sharipov.wdev@gmail.com>
2024-05-22 22:33:23 +0200

    Structure main function

--- Game/Player.cs
+++ Game/Player.cs
@@ -5,6 +5,10 @@ namespace Game;
 
 abstract class Player{
     protected List<Card> hand = new List<Card>();
+    
+    //Player should not know anything about the game, except open info.
+    //However, it is being passed for simplicity of implementation sake.
+    //TODO: change it, so the Player receives a clone state.
     public abstract Card? ChooseMove(Game game);
 
     public List<Card> GetCards(){
--- Program.cs
+++ Program.cs
@@ -1,8 +1,21 @@
 ﻿//TODO: Add game initialization and play 
 // (or maybe move it to App and just call Game.Begin() here and Console.ReadKey())
+using Domain;
+using App;
+using Game;
+using View;
+using System.Collections.Generic;
 
 public class Program(){
     static void Main(string[] args){
-        // Players?[] players = { null, new }
+        ConsoleAgent ca = new ConsoleAgent();
+        RandomAgent ra = new RandomAgent();
+        List<Player> players = new List<Player>{ca, ra};
+        Game.Game game = new Game.Game(players);
+        View.View view = new View.View();
+
+        App.App app = new App.App(game, view);
+
+        app.Run();
     }
 }
