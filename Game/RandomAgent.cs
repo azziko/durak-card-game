6a953fc642d4c2fb72db77cbb9852c884b2fee05
Azziko <sharipov.wdev@gmail.com>
2024-06-06 21:07:53 +0200

    Change randomAgent behavior to support passing

--- Game/RandomAgent.cs
+++ Game/RandomAgent.cs
@@ -10,6 +10,15 @@ class RandomAgent : Player {
     public override (EPlayerAction, Card?) ChooseMove(Game game) {
         List<Card?> validMoves = game.GetValidMoves();
 
+        int randomSkip = game.Rand.Next(100);
+        if(randomSkip < 20 && validMoves.Any(move => move == null)){
+            return(EPlayerAction.Move, null);
+        } else {
+            if(validMoves.Count > 1){
+                validMoves.Remove(null);
+            }
+        }
+
         return (
             EPlayerAction.Move, 
             validMoves[game.Rand.Next(validMoves.Count-1)]
